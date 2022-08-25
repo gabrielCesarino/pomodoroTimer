110e53cc9ebe8e1fd1422522ca9596cdde58e9cd
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2022-08-25 06:12:43 -0300

    style: starting w/ styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
     <>
-      <h1>Oi</h1>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
     </>
   );
 }
