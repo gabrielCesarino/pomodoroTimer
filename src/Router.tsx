e8fa952054f2545c76ce62538b5c94513709a0bb
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2022-08-25 07:42:38 -0300

    feat: default layout for routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      {/* Rotas dentro de rotas, somam seus paths, por isso
+      adicionamos um path com /, para que o layout seja aplicado
+      para todas as rotas da aplicação
+      */}
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
