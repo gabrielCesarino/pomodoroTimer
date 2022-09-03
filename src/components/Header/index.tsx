6c35f03f2cce966608ff6c3adc228cd500869b39
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2022-09-02 23:36:19 -0300

    style: header and layout initial style

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>logo</span>
+      <nav>
+        <a href="">history</a>
+        <a href="">profile</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
